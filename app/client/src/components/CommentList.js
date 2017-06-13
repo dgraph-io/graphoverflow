a4ef95445ce9dfde23a2e4fc5adcaabd39966d34
Sung Won Cho <mikeswcho@gmail.com>
2017-06-13 10:09:59 +1000

    Collapse comments by default

--- app/client/src/components/CommentList.js
+++ app/client/src/components/CommentList.js
@@ -3,20 +3,57 @@ import React from "react";
 import CommentItem from "./CommentItem";
 import "../assets/styles/CommentList.css";
 
-const CommentList = ({ comments, currentUser, onDeleteComment }) => {
-  return (
-    <ul className="comment-list">
-      {comments.map(comment => {
-        return (
-          <CommentItem
-            key={comment._uid_}
-            comment={comment}
-            currentUser={currentUser}
-            onDeleteComment={onDeleteComment}
-          />
-        );
-      })}
-    </ul>
-  );
-};
-export default CommentList;
+export default class CommentList extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      expanded: false
+    };
+  }
+
+  handleToggleExpanded = () => {
+    this.setState({ expanded: !this.state.expanded });
+  };
+
+  render() {
+    const { comments, currentUser, onDeleteComment } = this.props;
+    const { expanded } = this.state;
+
+    let commentsOnDisplay;
+    if (expanded) {
+      commentsOnDisplay = comments;
+    } else {
+      commentsOnDisplay = comments.slice(0, 4);
+    }
+
+    return (
+      <div>
+        <ul className="comment-list">
+          {commentsOnDisplay.map(comment => {
+            return (
+              <CommentItem
+                key={comment._uid_}
+                comment={comment}
+                currentUser={currentUser}
+                onDeleteComment={onDeleteComment}
+              />
+            );
+          })}
+        </ul>
+
+        <a
+          href="#toggle-expand"
+          onClick={e => {
+            e.preventDefault();
+            this.handleToggleExpanded();
+          }}
+        >
+          {expanded
+            ? <span>Collapse comments</span>
+            : <span>Show all {comments.length} comments</span>}
+        </a>
+      </div>
+    );
+  }
+}
