768c722d5fa84fa56091ae1e89c918d91fb61a56
Sung Won Cho <mikeswcho@gmail.com>
2017-06-16 22:07:19 -0700

    Fix

--- app/client/src/index.js
+++ app/client/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import AppProvider from "./AppProvider";
-import registerServiceWorker from "./registerServiceWorker";
+//import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(<AppProvider />, document.getElementById("root"));
-registerServiceWorker();
+// registerServiceWorker();
