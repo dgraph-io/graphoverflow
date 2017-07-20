708601f18bcc9786e4571ab24e1a8e09dfad40c5
Sung Won Cho <mikeswcho@gmail.com>
2017-07-20 16:14:01 +1000

    Add missing link from comments to user profile

--- app/client/src/components/CommentItem.js
+++ app/client/src/components/CommentItem.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import moment from "moment";
 import marked from "marked";
 
 const CommentItem = ({ comment, currentUser, onDeleteComment }) => {
   const isOwner = currentUser && comment.Author[0]._uid_ === currentUser._uid_;
+  const author = comment.Author[0];
 
   return (
     <li className="comment-item">
@@ -14,14 +16,13 @@ const CommentItem = ({ comment, currentUser, onDeleteComment }) => {
         <span
           className="comment-body"
           dangerouslySetInnerHTML={{ __html: marked(comment.Text) }}
-        />
-        {" "}
-        -
-        {" "}
+        />{" "}
+        -{" "}
         <span className="comment-author-name">
-          {comment.Author[0].DisplayName}
-        </span>
-        {" "}
+          <Link to={`/users/${author._uid_}`}>
+            {author.DisplayName}
+          </Link>
+        </span>{" "}
         <span className="comment-timestamp">
           {moment(comment.Timestamp).fromNow()}
         </span>
