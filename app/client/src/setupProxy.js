33da01a647014df023ae033fe5e44de9c0077a48
Michel Conrado <MichelDiz@users.noreply.github.com>
2020-04-26 22:55:37 -0300

    Bump everything

--- app/client/src/setupProxy.js
+++ app/client/src/setupProxy.js
@@ -1,5 +1,5 @@
-const proxy = require('http-proxy-middleware');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
-  app.use(proxy('/api', { target: 'http://localhost:3001/' }));
+  app.use(createProxyMiddleware('/api', { target: 'http://localhost:3001/' }));
 };
